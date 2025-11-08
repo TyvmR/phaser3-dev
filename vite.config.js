492cd6b495a20670dddcc12411a5fae00de42a38
john <john>
2025-11-08 14:31:48 +0000

    修改部分代码

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'docs'
-  }
+  },
+  server: {
+    host: true,         // 启用外部访问
+    port: 3000,         // 默认服务端口
+    strictPort: false,  // 自动处理端口占用
+    // 注意：allowedHosts 字段无需配置
+    allowedHosts: [
+      '460bc851532f42baa013f469bfb2785f--3000.ap-shanghai2.cloudstudio.club']
+}
 });
